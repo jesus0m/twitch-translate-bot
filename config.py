814c9fb40444c7b826b035f3c77ab22deec92f7e
Jesús Oliva Morales <jesusom@ext.inditex.com>
2023-12-20 00:13:51 +0100

    [ADDED] build.py and bot updated (#3)

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ TOKEN = ''  # Twitch OAuth token
 BOT_NAME = ''  # Bot's Twitch username
 CHANNEL = ''  # Channel to join
 
+WELCOME_MESSAGE = "Hello everyone! I'm a translation bot :D Use the command !translate to translate messages."
+
 # Translation Settings
 DEST_LANGUAGE = 'es'  # Target language for translations
 IGNORE_LANGUAGES = []  # List of language codes to ignore
